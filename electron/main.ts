113b71b53bb719ce03c70b82e0b1805a98f7fbe9
Jennifer Beidas <jenni.b@gmail.com>
2025-11-30 19:29:28 -0500

    Enables updates

--- electron/main.ts
+++ electron/main.ts
@@ -1,4 +1,5 @@
 import { app, BrowserWindow, ipcMain, Menu, shell, dialog, session, MenuItemConstructorOptions } from "electron";
+import { autoUpdater } from "electron-updater";
 import path from "path";
 import net from "net";
 
@@ -56,6 +57,65 @@ const createWindow = () => {
       contextIsolation: true, // Enable context isolation
       sandbox: true, // Re-enable sandbox for security
     },
+  });
+
+  // --- Auto Updater ---
+  // --- Auto Updater ---
+  autoUpdater.autoDownload = false;
+
+  // Check for updates once the window is ready
+  mainWindow.once('ready-to-show', () => {
+    autoUpdater.checkForUpdates();
+  });
+
+  // Optional: Log update events for debugging
+  autoUpdater.on('checking-for-update', () => {
+    console.log('Checking for update...');
+  });
+
+  autoUpdater.on('update-available', (info) => {
+    console.log('Update available:', info);
+    dialog.showMessageBox(mainWindow, {
+      type: 'info',
+      title: 'Update Available',
+      message: `A new version (${info.version}) is available. Do you want to download it now?`,
+      buttons: ['Yes', 'No']
+    }).then((result) => {
+      if (result.response === 0) { // 'Yes' button
+        autoUpdater.downloadUpdate();
+      }
+    });
+  });
+
+  autoUpdater.on('update-not-available', (info) => {
+    console.log('Update not available:', info);
+  });
+
+  autoUpdater.on('error', (err) => {
+    console.log('Error in auto-updater. ' + err);
+  });
+
+  autoUpdater.on('download-progress', (progressObj) => {
+    let log_message = "Download speed: " + progressObj.bytesPerSecond;
+    log_message = log_message + ' - Downloaded ' + progressObj.percent + '%';
+    log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')';
+    console.log(log_message);
+    // Optional: Send progress to renderer to show a progress bar
+    mainWindow.webContents.send('update-progress', progressObj.percent);
+  });
+
+  autoUpdater.on('update-downloaded', (info) => {
+    console.log('Update downloaded');
+    dialog.showMessageBox(mainWindow, {
+      type: 'info',
+      title: 'Update Ready',
+      message: 'Update downloaded. Application will be quit for update...',
+      buttons: ['Restart Now', 'Later']
+    }).then((result) => {
+      if (result.response === 0) { // 'Restart Now' button
+        autoUpdater.quitAndInstall();
+      }
+    });
   });
 
   // Add a handler for the 'toggle-fullscreen' event from the renderer
